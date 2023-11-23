f0634c31beed8c4f3272a38039ab3164e71f3014
Ducktatorrr <109146941+Ducktatorrr@users.noreply.github.com>
2023-11-23 16:19:11 +0100

    add actions and alerts basis

--- assets/js/158adc12.f25a8614.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs_connect_iot_nl=self.webpackChunkdocs_connect_iot_nl||[]).push([[587],{3769:c=>{c.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- assets/js/31af3226.983ef1a1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs_connect_iot_nl=self.webpackChunkdocs_connect_iot_nl||[]).push([[535],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
